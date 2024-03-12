a5d874bf157b4400a461331fe216e6ade88a3dcb
MatiasLudu <matuteldn@gmail.com>
2024-03-12 18:05:29 -0300

    First Commit

---
+++ assets/js/marquee.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let marquee = (selector_list, speed) => {
+        
+        const list = document.querySelector(selector_list);
+    
+        let move = 0;
+
+
+        setInterval(()=> {
+            list.style.marginLeft = `-${move}px`;
+
+            if(move > list.clientWidth){
+                move = 0;
+            }
+
+            move = move + speed;
+        }, 0);
+
+        
+    }
+
+    marquee(".marquee__list", 1);
+
+});
